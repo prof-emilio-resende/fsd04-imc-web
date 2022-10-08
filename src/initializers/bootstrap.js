3ce55f4d8f89c22d7185c747654a3aeaf4960919
Emilio Resende <emilio.murta@msitbrasil.com>
2022-10-07 21:15:06 -0300

    add scss/sass

--- src/initializers/bootstrap.js
+++ src/initializers/bootstrap.js
@@ -1,4 +1,4 @@
-import '../index.css';
+import '../index.scss';
 
 import ImcTableView from '../views/ImcTableView.js';
 import ImcTableController from '../controllers/ImcTableController.js'
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const plugins = [new MiniCssExtractPlugin()];
 
 const config = {
@@ -28,10 +29,14 @@ const config = {
                 }
             },
             {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             }
         ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new CssMinimizerPlugin()]
     }
 }
 
